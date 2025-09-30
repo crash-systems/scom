e6aa9a7f79f1cbcbc23c93d124a70cd8deb26c08
savalet <savinien.petitjean@gmail.com>
2025-09-30 17:16:06 +0200

    Fix version number

--- src/common.h
+++ src/common.h
@@ -20,7 +20,7 @@
     #define COMMON_H
     #include <termio.h>
 
-    #define VERSION "1.1"
+    #define VERSION "1.2"
     #define BIN_NAME "scom"
     #define OPT_FLAGS "hb:s:"
     #define CTRL_D_OCT_SEQ '\04'
